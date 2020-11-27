b65f2f34af75440c05a5a19f088999320125148c
CheoSYS-henry <josecar1805@gmail.com>
2020-11-27 13:03:51 -0500

    Modificacion de Comentarios
    esdd

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -74,7 +74,7 @@ function esEntero(numero) {
 function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3, devuelve "fizz"
   // Si "numero" es divisible entre 5, devuelve "buzz"
-  // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
+  // Si "numero" es divisible entre 3 y 5 (ambos) devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
   let div3=numero/3;
   let eva3=div3-Math.trunc(div3);
