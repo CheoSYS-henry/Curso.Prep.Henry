1fb7e0b4385c79717fcb087fafc933c16872ff81
CheoSYS-henry <josecar1805@gmail.com>
2020-12-09 11:12:47 -0500

    CP commiteado por CheoSYS-henry a las 1607530367735

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -18,7 +18,7 @@ function sumarArray(numeros, cb) {
 }
 
 function forEach(array, cb) {
-  // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
+  // Itera sobre la matriz "array y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   array.forEach(function(arre) {
     cb(arre);
